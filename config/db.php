c2715efd0507e84154de80f948757c3415fbd58b
Your Name <you@example.com>
2015-04-20 05:43:12 +0200

    modif db config

--- config/db.php
+++ config/db.php
@@ -2,8 +2,8 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=schools',
-    'username' => 'root',
-    'password' => '123456',
+    'dsn' => 'mysql:host=192.3.131.79;dbname=schools',
+    'username' => 'root_sch',
+    'password' => 'kG0OGb6DZ',
     'charset' => 'utf8',
 ];
